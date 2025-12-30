2ae13c41fd413a0adc84c56c66969738a362603d
Estacio <estaciodifabio@hotmail.com>
2025-12-30 18:11:19 -0300

    conection string

--- app/infrastructure/db/database.py
+++ app/infrastructure/db/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("postgresql://postgres:postgres@database_production:5432/lanchonete_producao")
+engine = create_engine("postgresql://gb_admin:#f3W^kLP}^4;!mF+?NL&W)w9@http://gb-dev-postgres.c6nea8wsy9hd.us-east-1.rds.amazonaws.com:5432/goodburger")
+#engine = create_engine("postgresql://postgres:postgres@database_production:5432/lanchonete_producao")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
